cab3cc52dcb0c1119f9605b5187fa5ff74e8664f
Christian Kolb <info@digital-craftsman.de>
2024-10-07 15:17:21 +0200

    Update phpunit

--- src/ValueObject/Exception/IdListDoesNotContainEveryId.php
+++ src/ValueObject/Exception/IdListDoesNotContainEveryId.php
@@ -4,7 +4,11 @@ declare(strict_types=1);
 
 namespace DigitalCraftsman\Ids\ValueObject\Exception;
 
-/** @psalm-immutable */
+/**
+ * @psalm-immutable
+ *
+ * @codeCoverageIgnore
+ */
 final class IdListDoesNotContainEveryId extends \DomainException
 {
     public function __construct()
